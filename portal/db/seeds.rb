9d48f4475a061e6fcd0690e3338c2582ce3ccccd
Maqsood <maqsood.ulde@gmail.com>
2020-04-26 21:14:54 +0530

    Added User model
    https://trello.com/c/QcvyMXZU/7-model-structure-for-post

--- portal/db/seeds.rb
+++ portal/db/seeds.rb
@@ -4,4 +4,8 @@
     ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
     instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
   )
+
 end
+
+
+100.times { FactoryBot.create(:user, :confirmed) }
--- portal/spec/factories/users.rb
+++ portal/spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    email      { Faker::Internet.email }
+    password   { Faker::Internet.password }
+
+    trait :confirmed do
+      before :create do |user|
+        user.skip_confirmation!
+        user.skip_confirmation_notification!
+      end
+    end
   end
 end
