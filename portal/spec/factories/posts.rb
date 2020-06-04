86c7f219554988cf51f30fa040f4be6d5ed45fbc
Maqsood <maqsood.ulde@gmail.com>
2020-06-05 01:12:59 +0530

    changed post

--- portal/spec/factories/posts.rb
+++ portal/spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
-    body { "MyText" }
-    state { "MyString" }
+    body { Faker::Lorem.paragraph(sentence_count: 3, supplemental: true, random_sentences_to_add: 2) }
+    state { 'default' }
     user { nil }
   end
 end
